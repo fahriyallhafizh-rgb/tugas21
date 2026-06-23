5658cd9d319804b1a3d2b773ea6504333ce5a617
fahriyallhafizh-rgb <fahriyallhafizh@gmail.com>
2026-06-23 13:46:54 +0700

    Delete soal5.js

--- soal5.js
+++
@@ -1,8 +0,0 @@
-let kataSatu = "";
-let kataDua = "BISA";
-
-let hasil = kataSatu + " " + kataDua;
-
-console.log("Kalimat Gabungan:", hasil);
-
-document.write("Kalimat Gabungan: " + hasil + "<br>");
